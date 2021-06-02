5712a100b445e82223d24c8072b583ad159de872
marwan <marwan.alsubayi@gmail.com>
2021-06-02 06:55:35 +0300

    second commit

--- addremove -element/add-remove.js
+++ addremove -element/add-remove.js
@@ -1,16 +1,15 @@
 let add=document.getElementById("add-element");
 let i=0;////for id element 
-let idnum=0;////id number text insid h3
+
 
 add.addEventListener("click" ,function(){
     i++;
-    idnum++;
     let mainelement=document.getElementById("elements");
     mainelement.insertAdjacentHTML("beforeEnd","<div class='element'><h3>Element  </h3><h4>This is an element</h4><button>Delete</button></div>")
 
    let element=mainelement.querySelectorAll(".element")[i-1];
-   idnumbertext=element.querySelector("h3").innerHTML="id number"+idnum;
-    element.id=idnum;
+   idnumbertext=element.querySelector("h3").innerHTML="id number"+i;
+    element.id=i;
 
    
 /////////////remove
@@ -20,7 +19,7 @@ let del =element.querySelector("button");
     
     //////if we remove first element ,the second element will change its id like the previous that has been removed.and so on for other elements.....
    
-    if(mainelement.childElementCount!=idnum);{
+    if(mainelement.childElementCount!=i);{
         for(count=1;count<mainelement.childElementCount+1;count++){
         let numelement=  mainelement.querySelectorAll(".element")[count-1];
         idnumbertext=numelement.querySelector("h3").innerHTML="id number"+ count;
@@ -29,7 +28,6 @@ let del =element.querySelector("button");
         
     }
     ////when we add element agin it will add after last number of child 
-    idnum=mainelement.childElementCount;
     i=mainelement.childElementCount;
     })
 
