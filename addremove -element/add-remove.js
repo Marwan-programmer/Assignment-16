a9e9f96d3d2fe24deaf8394e69e972e9b96043bb
marwan <marwan.alsubayi@gmail.com>
2021-06-02 06:36:12 +0300

    first commit

--- addremove -element/add-remove.js
+++ addremove -element/add-remove.js
@@ -1,22 +1,42 @@
 let add=document.getElementById("add-element");
-
+let i=0;////for id element 
+let idnum=0;////id number text insid h3
 
 add.addEventListener("click" ,function(){
-    let element=document.getElementById("elements");
-    element.insertAdjacentHTML("beforeEnd","<div class='element'><h3>Element </h3><h4>This is an element</h4><button>Delete</button></div>")
-    let del =document.querySelector(".element button");
-     del.onclick=remove;
+    i++;
+    idnum++;
+    let mainelement=document.getElementById("elements");
+    mainelement.insertAdjacentHTML("beforeEnd","<div class='element'><h3>Element  </h3><h4>This is an element</h4><button>Delete</button></div>")
 
-    
-})
+   let element=mainelement.querySelectorAll(".element")[i-1];
+   idnumbertext=element.querySelector("h3").innerHTML="id number"+idnum;
+    element.id=idnum;
 
-
-function remove(e){
-let del =document.querySelector(".element button");
+   
+/////////////remove
+let del =element.querySelector("button");
     del.addEventListener("click",function(){
+        element.remove();
     
-        let elementDel=document.querySelector(".element")
-         
-    elementDel.outerHTML="";
-    })}
+    //////if we remove first element ,the second element will change its id like the previous that has been removed.and so on for other elements.....
+   
+    if(mainelement.childElementCount!=idnum);{
+        for(count=1;count<mainelement.childElementCount+1;count++){
+        let numelement=  mainelement.querySelectorAll(".element")[count-1];
+        idnumbertext=numelement.querySelector("h3").innerHTML="id number"+ count;
+        numelement.id=count;
+         }
+        
+    }
+    ////when we add element agin it will add after last number of child 
+    idnum=mainelement.childElementCount;
+    i=mainelement.childElementCount;
+    })
+
+
+
+
+
+
+})
 
